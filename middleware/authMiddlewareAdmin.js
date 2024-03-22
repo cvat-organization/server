178b734c3af10f5ac289c5b5180fdfeb0ff5ab38
Rohan <rohan572003@gmail.com>
2024-03-22 12:24:36 +0530

    Added Users vs Time filter endpoint

---
+++ middleware/authMiddlewareAdmin.js
@@ -0,0 +1,33 @@
+// Authentication middleware to verify the session token FOR SUPERUSERS
+
+const jwt = require('jsonwebtoken');
+
+const users = require('../models/users');
+const config = require('../config/config');
+const secretKey = config.jwtSecret;
+
+async function authMiddlewareAdmin(req, res, next) {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+
+        // Check if token is present in request header
+        if (!token)
+            return res.status(400).json({message: 'Token missing'});
+
+        // Verify the token
+        const decodedToken = jwt.verify(token, secretKey);
+        const user = await users.findOne({_id: decodedToken._id, userType: 'Superuser'});
+        if (!(user && user.isActive && user.isLoggedIn))
+            return res.status(401).json({message: 'Unauthorized. Session not found'});
+
+        // Attach the decoded token to the request object for later use
+        req._id = decodedToken._id;
+        
+        next();
+    } catch (err) {
+        // Error handling
+        res.status(401).json({message: err.message});
+    }
+}
+
+module.exports = authMiddlewareAdmin;
---
+++ routes/admin/dashboard/get-users-vs-time.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+
+const authMiddlewareAdmin = require('../../middleware/authMiddlewareAdmin');
+const users = require('../../models/users');
+
+// Get users vs time data route
+router.get('/', authMiddlewareAdmin, async(req, res) => {
+    try{
+        const userType = req.body.userType;
+
+        // Check if all reqd fields are present
+        if (!userType)
+            return res.status(400).json({message: 'Invalid request body'});
+
+        // Fetch the users
+        const usersData = await users.find({ userType });
+
+        // Convert the user data to 2 arrays of userIDs and registration dates
+        const dateVSCount = {};
+        usersData.forEach((user) => {
+            if (!dateVSCount[user.createdAt])
+                dateVSCount[user.createdAt] = 0;
+            else
+                dateVSCount[user.createdAt] ++;
+        });
+
+        // Respond to the client with the user data
+        res.status(200).json({
+            message: 'Users vs time data fetched successfully',
+            dates: Object.keys(dateVSCount),
+            counts: Object.values(dateVSCount),
+        });
+
+    } catch(err){
+        res.status(500).json({message: err.message});
+    }
+});
+
+module.exports = router;
